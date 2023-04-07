920106da3b5d9a6131a6a8d947a2178f14112b46
Arthur <AmeliePoulain-92@mail.ru>
2023-04-07 20:57:59 +0300

    fix

--- services/newsletter.service.ts
+++ services/newsletter.service.ts
@@ -5,8 +5,9 @@ if (process.env.NODE_ENV !== 'production') {
     process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 }
 
+// TODO add robots.txt to public folder?
 export async function getNewsletterByGuid(guid: string): Promise<NewsLetter> {
-    const url = `${process.env.API_BASE_URL}/${guid}`;
+    const url = `https://api.jsonserve.com/${guid}`;
     const res = await fetch(url);
     const data = await res.json();
 
