0156bf60509753d713836650521ff85dbcf90989
Arthur <AmeliePoulain-92@mail.ru>
2023-04-07 18:40:20 +0300

    fix

--- pages/newsletters/[guid].tsx
+++ pages/newsletters/[guid].tsx
@@ -10,7 +10,7 @@ export const config: PageConfig = {
 import Newsletter from "@newsletter/components/content/Newsletter";
 // interfaces
 import { NewsLetter as NewsLetterInterface } from "@newsletter/utils/interfaces";
-import { getNewsletterByGuid } from "@newsletter/services/newsletter.service";
+// import { getNewsletterByGuid } from "@newsletter/services/newsletter.service";
 import MainLayout from "@newsletter/layouts/MainLayout";
 import NewsletterWrapper from "@newsletter/components/layout/NewsletterWrapper";
 import sortData from "@newsletter/utils/sortData";
@@ -29,10 +29,25 @@ Newsletters.getLayout = function (page: ReactElement) {
 
 // This gets called on every request
 export async function getServerSideProps(context: any) {
+  const isDevENV = process.env.NODE_ENV !== 'production';
+  const API_BASE_URL = isDevENV ? 'https://localhost:5001' : 'https://adminapi.bevnet.com';
+  async function getNewsletterByGuid(guid: string) {
+    const url = `${API_BASE_URL}/Newsletters/public/${guid}`;
+    const res = await fetch(url, {
+        headers: {
+            "content-type": "application/json",
+            "Access-Control-Allow-Origin": API_BASE_URL,
+        },
+    });
+    const data = await res.json();
+
+    return data;
+} 
+
   const { params } = context;
   try {
-    // const data = await getNewsletterByGuid(params.guid);
-    const data = (await fetch('https://api.jsonserve.com/E_PBU4')).json();
+    const data = await getNewsletterByGuid(params.guid);
+    // const data = (await fetch('https://api.jsonserve.com/E_PBU4')).json();
     // @ts-ignore
     const sortedData = sortData(data);
 
