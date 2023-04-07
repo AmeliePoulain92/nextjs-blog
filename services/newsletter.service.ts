3fe11e753afd7ba6235a3009b7e1a96563d0c3bf
Arthur <AmeliePoulain-92@mail.ru>
2023-04-07 18:54:35 +0300

    fix

--- services/newsletter.service.ts
+++ services/newsletter.service.ts
@@ -1,8 +1,16 @@
-// import { API_BASE_URL } from "@newsletter/config";
+import { API_BASE_URL } from "@newsletter/config";
 import { NewsLetter } from "@newsletter/utils/interfaces";
 
 // TODO remove use editionId:10848 to create more items
-export async function getNewsletterByGuid(guid: string) {
+export async function getNewsletterByGuid(guid: string): Promise<NewsLetter> {
     const url = `https://api.jsonserve.com/${guid}`;
-    return (await fetch(url)).json();
+    const res = await fetch(url, {
+        headers: {
+            "content-type": "application/json",
+            "Access-Control-Allow-Origin": API_BASE_URL,
+        },
+    });
+    const data = await res.json();
+
+    return data;
 } 
