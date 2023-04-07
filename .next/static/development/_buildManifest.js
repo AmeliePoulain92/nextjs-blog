e49d0bfda8921dc839d0fad1ab593b655e37a571
Arthur <AmeliePoulain-92@mail.ru>
2023-04-07 19:57:02 +0300

    fix

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/newsletters/[guid]":["static\u002Fchunks\u002Fpages\u002Fnewsletters\u002F[guid].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fnewsletters\u002F[guid]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
