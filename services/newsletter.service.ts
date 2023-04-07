79c14cb9c94f0b2161cde63ec08a97300f7b6cd2
Arthur <AmeliePoulain-92@mail.ru>
2023-04-07 18:50:05 +0300

    fix

--- services/newsletter.service.ts
+++ services/newsletter.service.ts
@@ -4,8 +4,5 @@ import { NewsLetter } from "@newsletter/utils/interfaces";
 // TODO remove use editionId:10848 to create more items
 export async function getNewsletterByGuid(guid: string): Promise<NewsLetter> {
     const url = `${API_BASE_URL}/Newsletters/public/${guid}`;
-    const res = await fetch(url);
-    const data = await res.json();
-
-    return data;
+    return (await fetch(url)).json();
 } 
