e878a1be5f7d5095c240da47bc0210185c6535b0
Arthur <AmeliePoulain-92@mail.ru>
2023-04-07 19:51:25 +0300

    fix

--- services/newsletter.service.ts
+++ services/newsletter.service.ts
@@ -1,15 +1,13 @@
-import { API_BASE_URL } from "@newsletter/config";
 import { NewsLetter } from "@newsletter/utils/interfaces";
 
-// TODO remove use editionId:10848 to create more items
+// bypassing SSL certificate locally
+if (process.env.NODE_ENV !== 'production') {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+}
+
 export async function getNewsletterByGuid(guid: string): Promise<NewsLetter> {
-    const url = `${process.env.API_BASE_URL}/${guid}`;
-    const res = await fetch(url, {
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": API_BASE_URL,
-        },
-    });
+    const url = `${process.env.API_BASE_URL}/Newsletters/public/${guid}`;
+    const res = await fetch(url);
     const data = await res.json();
 
     return data;
