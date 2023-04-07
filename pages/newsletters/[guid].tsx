099fbc295c7a32e302ccd5bb4b76fea9a98070d5
Arthur <AmeliePoulain-92@mail.ru>
2023-04-07 18:48:44 +0300

    fix

--- pages/newsletters/[guid].tsx
+++ pages/newsletters/[guid].tsx
@@ -10,7 +10,7 @@ export const config: PageConfig = {
 import Newsletter from "@newsletter/components/content/Newsletter";
 // interfaces
 import { NewsLetter as NewsLetterInterface } from "@newsletter/utils/interfaces";
-// import { getNewsletterByGuid } from "@newsletter/services/newsletter.service";
+import { getNewsletterByGuid } from "@newsletter/services/newsletter.service";
 import MainLayout from "@newsletter/layouts/MainLayout";
 import NewsletterWrapper from "@newsletter/components/layout/NewsletterWrapper";
 import sortData from "@newsletter/utils/sortData";
@@ -29,26 +29,9 @@ Newsletters.getLayout = function (page: ReactElement) {
 
 // This gets called on every request
 export async function getServerSideProps(context: any) {
-  const isDevENV = process.env.NODE_ENV !== 'production';
-  const API_BASE_URL = isDevENV ? 'https://api.jsonserve.com/E_PBU4' : 'https://api.jsonserve.com/E_PBU4';
-  const getNewsletterByGuid = async function getNewsletterByGuid(guid: string) {
-    const url = `${API_BASE_URL}/Newsletters/public/${guid}`;
-    const res = await fetch(url, {
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": API_BASE_URL,
-        },
-    });
-    const data = await res.json();
-
-    return data;
-} 
-
   const { params } = context;
   try {
     const data = await getNewsletterByGuid(params.guid);
-    // const data = (await fetch('https://api.jsonserve.com/E_PBU4')).json();
-    // @ts-ignore
     const sortedData = sortData(data);
 
     return {
--- services/newsletter.service.ts
+++ services/newsletter.service.ts
@@ -4,12 +4,7 @@ import { NewsLetter } from "@newsletter/utils/interfaces";
 // TODO remove use editionId:10848 to create more items
 export async function getNewsletterByGuid(guid: string): Promise<NewsLetter> {
     const url = `${API_BASE_URL}/Newsletters/public/${guid}`;
-    const res = await fetch(url, {
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": API_BASE_URL,
-        },
-    });
+    const res = await fetch(url);
     const data = await res.json();
 
     return data;
