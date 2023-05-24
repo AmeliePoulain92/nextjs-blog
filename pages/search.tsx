c52a182e565871efb9c9b1ce9b9575e48199543a
Arthur <AmeliePoulain-92@mail.ru>
2023-05-24 16:53:13 +0300

    fix

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,18 +1,9 @@
-import { AppBar, Toolbar } from "@material-ui/core";
 import SearchInput from "@newsletter/components/content/search/SearchInput";
-import { UserMenu } from "@newsletter/components/layout/components/UserMenu/UserMenu";
 
 export default function SearchNewsletters() {
   
   return (
     <>
-    <AppBar
-      position="sticky"
-    >
-      <Toolbar>
-      <UserMenu />
-      </Toolbar>
-    </AppBar>
       <SearchInput />
     </>
   );
